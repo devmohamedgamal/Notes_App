7ecc178a122005ab9a7d06c3d89f7def7664104c
devmohamedgamal <mpedo2010@gmail.com>
2023-07-26 20:46:10 +0300

    create note list view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'widgets/custom_app_bar.dart';
+import 'widgets/custom_note_item.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -9,16 +10,34 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 28),
+        padding: EdgeInsets.symmetric(horizontal: 28),
         child: Column(
           children: [
             SizedBox(
               height: 50,
             ),
             CustomAppBar(),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(child: NoteListView()),
           ],
         ),
       ),
     );
+  }
+}
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, index) {
+      return const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: CustomNoteItem(),
+      );
+    });
   }
 }
---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/Material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 28, bottom: 28, left: 16, right: 10),
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFCD7B),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build Your Career With Mohamed Gamal',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                )),
+          ),
+           Text(
+            'May 21, 2023',                             
+            style: TextStyle(color: Colors.black.withOpacity(0.4)),
+          ),
+        ],
+      ),
+    );
+  }
+}
